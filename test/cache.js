a980532f01f6f0d5bb560d6dc552ee1a61978aa8
Shane Thacker <shane@steadymade.com>
2016-08-22 22:51:12 -0500

    add defaults for json settings, make middleware names singular, fix sendStatus bug, add dynamic settings and override for res.json

--- test/cache.js
+++ test/cache.js
@@ -1,30 +1,32 @@
 var expect = require('expect.js');
-var Memcache = require('../lib/middleware/memcache')({
+var Cache = require('../lib/middleware/memcache')({
     settings: {
-        memcache: [{
-            url: '/foo',
-            expires: false
-        }]
+        cache: {
+            urls: [{
+                url: '/foo',
+                expires: false
+            }]
+        }
     }
 });
 
 
 
-describe('Memcache', function(){
+describe('Cache', function(){
 
     describe('createLookup', function(){
         it('should create a lookup object', function(){
-            expect(Memcache.lookup).to.be.an('object');
+            expect(Cache.lookup).to.be.an('object');
         });
     });
 
     describe('add', function(){
         it('should add a url to for caching to the lookup object', function(){
-            Memcache.add({
+            Cache.add({
                 url: '/boo',
                 expires: false
             });
-            expect(Memcache.lookup['/boo'].url).to.be('/boo');
+            expect(Cache.lookup['/boo'].url).to.be('/boo');
         });
     });
 });
--- test/redirect.js
+++ test/redirect.js
@@ -1,39 +1,39 @@
 var expect = require('expect.js');
-var Redirects = require('../lib/middleware/redirects')({
+var Redirect = require('../lib/middleware/redirect')({
     settings: {
-        redirects: {
+        redirect: {
             '/foo': '/bar'
         }
     }
 });
 
-describe('Redirects', function(){
+describe('Redirect', function(){
     describe('lookup', function(){
 
         describe('when url is present', function(){
             it('returns the lookup value for the url', function(){
-                expect(Redirects.lookup['/foo']).to.be('/bar');
+                expect(Redirect.lookup['/foo']).to.be('/bar');
             });
         });
 
         describe('when url is not present', function(){
             it('returns undefined', function(){
-                expect(Redirects.lookup['/jack']).to.be(undefined);
+                expect(Redirect.lookup['/jack']).to.be(undefined);
             });
         });
     });
 
     describe('add', function(){
         it('adds a "/from" property to the lookup table', function(){
-            Redirects.add('/from', '/to');
-            expect(Redirects.lookup['/from']).to.be('/to');
+            Redirect.add('/from', '/to');
+            expect(Redirect.lookup['/from']).to.be('/to');
         });
     });
 
     describe('remove', function(){
         it('should not have "/from" property in lookup table after removing', function(){
-            Redirects.remove('/from');
-            expect(Redirects.lookup).not.to.have.property('/from');
+            Redirect.remove('/from');
+            expect(Redirect.lookup).not.to.have.property('/from');
         });
     });
 
@@ -42,29 +42,29 @@ describe('Redirects', function(){
             var lookup = {
                 '/new': '/bar'
             };
-            Redirects.reset(lookup);
-            expect(Redirects.lookup).to.be(lookup);
+            Redirect.reset(lookup);
+            expect(Redirect.lookup).to.be(lookup);
         });
     });
 
     describe('empty', function(){
         it('should empty lookup table', function(){
-            Redirects.empty();
-            expect(Redirects.lookup).to.be.empty();
+            Redirect.empty();
+            expect(Redirect.lookup).to.be.empty();
         });
     });
 
     describe('redirect to simple match', function(){
         it('should redirect from to empty lookup table', function(){
 
-            Redirects.empty();
-            expect(Redirects.lookup).to.be.empty();
+            Redirect.empty();
+            expect(Redirect.lookup).to.be.empty();
         });
     });
 
-    describe('handler', function(){
+    describe('handle', function(){
         it('should redirect to ', function(){
-            Redirects.handler({
+            Redirect.handle({
                     url: '/foo',
                     host: 'www.steadymade.com:3000',
                     hostname: 'www.steadymade.com'
